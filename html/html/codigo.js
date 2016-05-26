54f8317abc99bd231ebaebc06b4686bd46a5a52d
jhon capera <jhonkpera@gmail.com>
2016-05-26 10:28:16 -0500

    Ejercicios

---
+++ html/html/codigo.js
@@ -0,0 +1,10 @@
+var nombre = promt('cual es su nombre?');
+if (nombre == 'jhon kpera') 
+{
+	alert('bienvenidos: ' +nombre+' el goleador');
+}
+ else 
+ {
+	alert('bienvenido: ' +nombre+' el defesa');
+
+} 
